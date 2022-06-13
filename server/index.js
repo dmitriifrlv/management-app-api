7f5c07df92f6ef0b1bd8699fab87dc4f4c42b563
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-06-12 20:55:37 -0400

    setup graphql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const port = process.env.PORT || 5500;
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 const app = express();
 
 app.listen(port, console.log("server "));
